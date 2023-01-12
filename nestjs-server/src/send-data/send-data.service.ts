3d9a14ade26224bfb1130b34483394ec5aa6b340
ishutkoff <ishutkoff@yandex.ru>
2023-01-12 12:37:59 +0300

    Поменял порядок полей в сообщении отправляемом формой

--- nestjs-server/src/send-data/send-data.service.ts
+++ nestjs-server/src/send-data/send-data.service.ts
@@ -15,13 +15,20 @@ export class SendDataService {
     const shop = await this.shopsService.getShopById(dataObject.shopId);
     const apiKey = shop.telegramApiKey;
     const chatId = shop.chatId;
-    let html = `<strong>Состав заказа:</strong>%0A`;
+    let html = `<strong>Данные покупателя:</strong>%0A`;
+    html += `Имя: ${dataObject.name}%0A`;
+    html += `Телефон: ${dataObject.phone}%0A`;
+    html += `<strong>Состав заказа:</strong>%0A`;
     dataObject.orderList.forEach((item) => {
       html += `<strong>${item.title}</strong> - ${item.price} ₽%0A`;
     });
-    html += `<strong>Данные покупателя:</strong>%0A`;
-    html += `Имя: ${dataObject.name}%0A`;
-    html += `Телефон: ${dataObject.phone}%0A`;
+    html += `<strong>Сумма заказа:</strong> ${dataObject.orderList
+      .reduce(
+        (accumulator, currentValue) => accumulator + currentValue.price,
+        0,
+      )
+      .toLocaleString('ru')} ₽`;
+
     await this.httpService
       .get(
         `https://api.telegram.org/bot${apiKey}/sendMessage?chat_id=${chatId}&text=${html}&parse_mode=html`,
